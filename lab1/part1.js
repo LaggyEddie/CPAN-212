52fd5059e41b3823c6aa4ca954493dfda5cb6c40
Eduardo Lee <leeeduardo924@gmail.com>
2024-09-12 22:11:00 -0400

    poop

---
+++ lab1/part1.js
@@ -0,0 +1,11 @@
+const yomama = require("lodash")
+
+const holidays = [
+    { name: "Chirstmas", date: new Date("2024-12-25")},
+    { name: "Canada Day", date: new Date("2025-07-01")},
+    { name: "New Year's Day", date: new Date("2025-01-01")},
+    { name: "Chirstmas", date: new Date("2024-12-25")},
+];
+
+const today = new Date();
+
