ec2b65bd941db7d39493863b97d326861f55554d
Eduardo Lee <leeeduardo924@gmail.com>
2024-09-19 21:45:23 -0400

    tiktok brain rot

---
+++ lab2/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8964;
+const lab_router = require("./router")
+
+app.use(express.urlencoded.apply({extended: true}));
+
+app.use("/lab2", lab_router)
+
+app.get("/", (req,res)=>{
+    res.send("HI BRO");
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
+
+app.use("", (req,res)=>{
+    res.status(404).send("Page not found")
+})
---
+++ lab2/router.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.send("The lab 2 router file")
+})
+
+router.get("/name", (req,res) => {
+    res.send("SOmeone (maybe)")
+})
+
+router.get("/greeting", (req,res) => {
+    res.send("The lab 2 router file")
+})
+
+router.get("/add/:a/:b", (req,res) => {
+    console.log(req.params);
+    let a = parseFloat(req.params.a);
+    let b = parseFloat(req.params.b);
+    res.send(JSON.stringify(a +b));
+})
+
+router.get("/calculate/:x/:y/:op", (req,res) => {
+    console.log(req.params);
+    let x = parseFloat(req.params.x);
+    let y = parseFloat(req.params.y);
+    let op = req.params.op;
+
+    switch (op) {
+        case "+":
+            res.send(JSON.stringify(x + y));
+            break;
+        case "-":
+            res.send(JSON.stringify(x - y));
+            break;
+        case "*":
+            res.send(JSON.stringify(x * y));
+            break;
+        case "/":
+            res.send(JSON.stringify(x / y));
+            break;
+        case "**":
+            res.send(JSON.stringify(x ** y));
+            break;
+    }
+})
+
+module.exports = router;
